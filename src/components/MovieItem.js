fc99540e0748073c6cf759e0f603247e2cd1840d
Conor <cwilson0005@hotmail.co.uk>
2019-02-20 11:56:39 +0000

    adding css to movie item

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./MovieItem.css"
 
 const MovieItem = ({children}) => (
-  <div className="movie-item">
+  <div className="media-card">
     <h4>{children}</h4>
   </div>
 );
